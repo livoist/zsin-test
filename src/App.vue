1dfc636c193a0b9065851eaff735f742424b563c
lai Ben <waterblue104@gmail.com>
2023-09-17 15:38:52 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  <div>
+    <MenuList />
+  </div>
+  <NineSquare />
+</div>
+</template>
+
+<script setup lang="ts">
+import { MenuList, NineSquare } from '../src/components'
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import MenuList from './MenuList.vue'
+import NineSquare from './NineSquare.vue'
+
+export {
+  MenuList,
+  NineSquare
+}
