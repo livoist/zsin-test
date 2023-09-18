0c71efe38006cfac064865ed36fc400854b52ed3
lai Ben <waterblue104@gmail.com>
2023-09-18 23:17:08 +0800

    add basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,64 @@
 <template>
 <div>
-  <div>
-    <MenuList />
+  <div class="navbar" @click.self="isOpenMenu = false">
+    <div class="menuBtn" @click="isOpenMenu = true"></div>
+    <SideMenu
+      :isOpenMenu="isOpenMenu"
+      :list="mockData"
+    />
   </div>
-  <NineSquare />
+  <div class="nineSquareContainer" @click="isOpenMenu = false">
+    <NineSquare />
+  </div>
 </div>
 </template>
 
 <script setup lang="ts">
-import { MenuList, NineSquare } from '../src/components'
+import { ref } from 'vue'
+import { SideMenu, NineSquare } from '../src/components'
+import { mockData } from './data'
+
+const isOpenMenu = ref(false)
 
 </script>
 
 <style scoped>
+.navbar {
+  background: #000;
+  width: 100vw;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 10px 2vw;
+  box-sizing: border-box;
+}
 
+.menuBtn {
+  border-top: 1px solid #fff;
+  border-bottom: 1px solid #fff;
+  width: 30px;
+  height: 16px;
+  position: relative;
+  cursor: pointer;
+}
+
+.menuBtn:after {
+  content: '';
+  position: absolute;
+  width: 100%;
+  height: 1px;
+  background: #fff;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.nineSquareContainer {
+  width: 100vw;
+  height: calc(100vh - 38px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+}
 </style>
